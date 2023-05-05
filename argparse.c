2c47b173e51dfcf1651785634a882185646651b6
Preston-D <prestonduffield1@gmail.com>
2023-05-05 08:31:22 -0700

    Arg parse changes

--- argparse.c
+++ argparse.c
@@ -11,69 +11,98 @@
 #define FALSE (0)
 #define TRUE (1)
 
-/*
- * argCount is a helper function that takes in a String and returns the number of "words" in the string assuming that whitespace is the only possible delimeter.
- */
+// argCount is a helper function that takes in a String and returns the number of "words" in the string,
+// assuming that whitespace is the only possible delimiter.
 static int argCount(char *line)
 {
   int count = 0;
   int in_word = FALSE;
 
-  // Iterate through the characters in the input string updating the counter and the in_word flag.
-  while (*line != '\0')
+  if (line != NULL)
   {
-    // whitespace
-    if (isspace(*line))
+    // Iterate through the characters in the string
+    while (*line != '\0')
     {
-      in_word = FALSE;
-    }
-    // non-whitespace
-    else
-    {
-      if (!in_word)
+      // If the current character is a whitespace, set in_word to FALSE
+      if (isspace(*line))
       {
-        count++;
+        in_word = FALSE;
       }
-      in_word = TRUE;
+      else
+      {
+        // If not in a word, increment the count and set in_word to TRUE
+        if (!in_word)
+        {
+          count++;
+        }
+        in_word = TRUE;
+      }
+      line++;
     }
-    line++;
   }
+
   return count;
 }
 
-/*
- *
- * Argparse takes in a String and returns an array of strings from the input.
- * The arguments in the String are broken up by whitespaces in the string.
- * The count of how many arguments there are is saved in the argcp pointer
- */
+// Argparse takes in a String and returns an array of strings from the input.
+// The arguments in the String are broken up by whitespaces in the string.
+// The count of how many arguments there are is saved in the argcp pointer.
 char **argparse(char *line, int *argcp)
 {
-  // Determine the number of arguments in the input string.
-  *argcp = argCount(line);
-  // Allocates memory for an array of pointers to strings (args), with one additional element to store a NULL pointer at the end.
-  char **args = (char **)malloc((*argcp + 1) * sizeof(char *));
+  char **args = NULL;
 
-  // Iterate through the input string, skipping any leading whitespace characters.
-  for (int i = 0; i < *argcp; i++)
+  // Check if both input pointers (line and argcp) are not NULL before processing
+  if (line != NULL && argcp != NULL)
   {
-    while (isspace(*line))
+    *argcp = argCount(line);
+    // Allocate memory for the argument array, with an additional element for the NULL terminator
+    args = (char **)malloc((*argcp + 1) * sizeof(char *));
+
+    // If memory allocation is successful, process the arguments
+    if (args != NULL)
     {
-      line++;
+      // Iterate through the arguments
+      for (int i = 0; i < *argcp; i++)
+      {
+        // Skip leading whitespaces
+        while (isspace(*line))
+        {
+          line++;
+        }
+
+        // Find the length of the current argument
+        int arg_len = 0;
+        while (line[arg_len] != '\0' && !isspace(line[arg_len]))
+        {
+          arg_len++;
+        }
+
+        // Allocate memory for the current argument string, including space for the NULL terminator
+        args[i] = (char *)malloc((arg_len + 1) * sizeof(char));
+
+        // If memory allocation is successful, copy the argument string and add the NULL terminator
+        if (args[i] != NULL)
+        {
+          strncpy(args[i], line, arg_len);
+          args[i][arg_len] = '\0';
+        }
+
+        // Move the line pointer to the next argument
+        line += arg_len;
+      }
+
+      // Add the NULL terminator to the argument array
+      args[*argcp] = NULL;
     }
-    int arg_len = 0;
-    while (line[arg_len] != '\0' && !isspace(line[arg_len]))
-    {
-      arg_len++;
-    }
-    // Calculates the length of the argument, allocates memory for the argument.
-    args[i] = (char *)malloc((arg_len + 1) * sizeof(char));
-    // Copies the argument's characters into the allocated memory.
-    strncpy(args[i], line, arg_len);
-    args[i][arg_len] = '\0';
-    line += arg_len;
   }
-  // Set the last element of the args array to NULL and return the array.
-  args[*argcp] = NULL;
+  else
+  {
+    // If argcp is not NULL, set it to 0 to indicate an error
+    if (argcp != NULL)
+    {
+      *argcp = 0;
+    }
+  }
+
   return args;
 }
